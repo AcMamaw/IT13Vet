4d489ef639cf42a59f1b751ac9d7213c9917b1be
AcMamaw <usakamillion@gmail.com>
2025-09-27 11:38:21 +0800

    Add project files.

---
+++ VeterinarianEMS/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using MahApps.Metro.Controls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace VeterinarianEMS
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LogoutButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You have been logged out.");
+        }
+
+        // Sidebar button click handlers
+        private void DashboardButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentArea.Content = new DashboardControl();
+        }
+
+        private void AttendanceButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentArea.Content = new AttendanceControl();
+        }
+
+        private void EmployeeButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Replace with your EmployeeControl
+            MainContentArea.Content = new EmployeeControl();
+        }
+
+        private void LeaveRequestButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentArea.Content = new LeaveRequestControl();
+        }
+
+        private void OvertimeRequestButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentArea.Content = new OvertimeRequestControl();
+        }
+
+        private void RolesButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentArea.Content = new RolesControl();
+        }
+
+        private void ShiftsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentArea.Content = new ShiftsControl();
+        }
+
+        private void PayrollButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentArea.Content = new PayrollControl();
+        }
+    }
+}
